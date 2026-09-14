d83b26bfabaa1476c20bc706e10d20dc6ba3d0d4
Sanatiel Gomes <sanatielsg@gmail.com>
2026-09-14 20:20:41 -0300

    incluindo .js; incluindo opcoes do menu

--- index.php
+++ index.php
@@ -1,2 +1,38 @@
-<?php echo "Iniciando projeto..."; ?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Pedidos e Estoque</title>
+    <script language=javascript src=index.js></script>
+</head>
+<body bgcolor="#ffffaa">
+    <div>
+        <ul>
+            <li><a href="?op=login">Login</a></li>
+            <li><a href="?op=rec-senha">Recuperação de senha</a></li>
+            <li><a href="?op=cad-usuario">Cadastros de Usuários</a></li>
+            <li><a href="?op=cad-empresa">Cadastros de Empresas</a></li>
+        </ul>
+    </div>        
+    <div style="display:none;">
+        <ul>
+            <li><a href="?op=cad-produto">Cadastros de Produtos</a></li>
+            <li><a href="?op=cad-cliente">Cadastros de Clientes</a></li>
+            <li><a href="?op=cad-caixa">Cadastros de Caixas</a></li>
+            <li><a href="?op=cad-banco">Cadastros de Bancos</a></li>
+        </ul> 
+    </div>
+    <div style="display:none;">
+        <ul>
+            <li><a href="?op=lan-estoque">Lançamentos de Estoques</a></li>
+            <li><a href="?op=lan-caixa">Lançamentos de Caixas</a></li>
+            <li><a href="?op=lan-banco">Lançamentos de Bancos</a></li>
+            <li><a href="?op=pedido-venda">Lançamentos Pedidos de Venda</a></li>
+            <li><a href="?op=lan-cr">Lançamentos de Contas a Receber</a></li>
+        </ul>
+    </div>
+    <hr>
+</body>
+</html>
 
